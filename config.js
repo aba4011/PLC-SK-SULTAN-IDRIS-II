7039833f261b01cb8d263ded1179ca5b6ba61279
aba4011 <sekolah-454@moe-dl.edu.my>
2026-08-15 09:56:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // CONFIGURATION FILE
 const CONFIG = {
   // URL Web App asas Google Apps Script baharu
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxYFpddPVJ0doi0VCNJavy6YWaJfVdzXjQ4xCXyGmAhW5l_6dbiRPzxInCXGt1VLV_DYA/exec"
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzXjPIW7ORNpZ1kyVkh9zR0vxHDrCJ66uuMcEXOX4-vzWC0NJyXSMijtMd1CS59vR0I0Q/exec"
 };
