ddb8f02f81d49530cb8f3871b155ea42452a192b
aba4011 <sekolah-454@moe-dl.edu.my>
2026-08-15 09:42:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // CONFIGURATION FILE
 const CONFIG = {
-  // Gantikan dengan Apps Script Exec URL anda selepas Deployment
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw7x0um9Dsj1a1-3aMouS0exRA9faXa0MqFXh3qbHkzIg9Z-1zEr-he42b9JQuVpSFxbQ/exec?action=getTeachers"
+  // URL Web App asas Google Apps Script (tanpa sebarang ?action=getTeachers di belakang)
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw7x0um9Dsj1a1-3aMouS0exRA9faXa0MqFXh3qbHkzIg9Z-1zEr-he42b9JQuVpSFxbQ/exec"
 };
